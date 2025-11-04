8a99d70faaf7d998e49943a5925d7753583c1c52
Jarod Smith <lbriones.dev@gmail.com>
2025-11-04 10:40:46 -0300

    Upd/ Actualiza datos de la cabecera de Swagger

--- src/main/java/com/onlyflans/bakery/config/SwaggerConfig.java
+++ src/main/java/com/onlyflans/bakery/config/SwaggerConfig.java
@@ -1,6 +1,7 @@
 // Swagger by Luis :3
 
 package com.onlyflans.bakery.config;
+import io.swagger.v3.oas.models.info.Contact;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,15 +10,21 @@ import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
 public class SwaggerConfig {
-    
+
     @Bean
-    public OpenAPI customOpenAPI(){
+    public OpenAPI customOpenAPI() {
+        String descripcion = """
+            Documentación de la API para el proyecto de la página <b>OnlyFlans 🍰</b><br><br>
+            <b>Desarrollado por:</b>Andrés Ortega, Leonel Briones y Luis Maulen<br>
+            <a href='mailto:contacto@onlyflans.cl'>contacto@onlyflans.cl</a> | 
+            <a href='https://onlyflans2.netlify.app' target="_blank">https://onlyflans2.netlify.app</a>
+            """;
+
         return new OpenAPI()
-            .info(new Info()
-                    .title("API OnlyFlans project")
-                    .version("1.0.5")
-                    .description("Documentación de la API para el proyecto de la pagina OnlyFlans")
-                    //.contact("Developed by: Leonel Briones, Luis Maulen and Andres Ortega")
-                    );
+                .info(new Info()
+                        .title("API OnlyFlans Project")
+                        .version("1.0.6")
+                        .description(descripcion)
+                );
     }
 }
