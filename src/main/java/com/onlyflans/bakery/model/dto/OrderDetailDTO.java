c978ebb23708d6a5efe1fdad00898f3affe30b80
Andrés Ortega <andythe20xd@gmail.com>
2025-11-07 14:39:15 -0300

    add/ mappers terminados

--- src/main/java/com/onlyflans/bakery/model/dto/OrderDetailDTO.java
+++ src/main/java/com/onlyflans/bakery/model/dto/OrderDetailDTO.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class OrderDetailDTO {
     private String id;
+    private String orderId;
     private String productoCodigo;
     private String nombreProducto;
     private Integer cantidad;
--- src/main/java/com/onlyflans/bakery/model/mapper/OrderMapper.java
+++ src/main/java/com/onlyflans/bakery/model/mapper/OrderMapper.java
@@ -1,4 +1,41 @@
 package com.onlyflans.bakery.model.mapper;
 
+import com.onlyflans.bakery.model.Order;
+import com.onlyflans.bakery.model.dto.OrderDTO;
+import com.onlyflans.bakery.model.dto.OrderDetailDTO;
+
+import java.util.Collections;
+import java.util.stream.Collectors;
+
 public class OrderMapper {
+
+    public static OrderDTO toDTO(Order order){
+
+        if (order == null) return null;
+
+        OrderDTO dto = new OrderDTO();
+        dto.setId(order.getId());
+        dto.setFecha(order.getFecha());
+        dto.setEstado(order.getEstado());
+        dto.setTotal(order.getTotal());
+        dto.setRutUsuario(order.getUser().getRut());
+
+        dto.setOrderDetails(
+                order.getOrderDetails() == null ?
+                        Collections.emptyList() :
+                        order.getOrderDetails().stream().map(orderDetail -> {
+                            OrderDetailDTO detailDTO =  new OrderDetailDTO();
+                            detailDTO.setId(orderDetail.getId());
+                            detailDTO.setProductoCodigo(orderDetail.getProduct().getCodigo());
+                            detailDTO.setNombreProducto(orderDetail.getProduct().getNombre());
+                            detailDTO.setCantidad(orderDetail.getCantidad());
+                            detailDTO.setSubtotal(orderDetail.getSubtotal());
+                            return detailDTO;
+                        }).collect(Collectors.toList())
+        );
+
+
+
+        return dto;
+    }
 }
