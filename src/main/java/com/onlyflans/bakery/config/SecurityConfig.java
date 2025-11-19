6540c8f6e47b077f1abc78a3337ba56e84537bf7
Jarod Smith <lbriones.dev@gmail.com>
2025-11-18 23:37:22 -0300

    Upd: Se deja publica la ruta de products para pruebas

--- src/main/java/com/onlyflans/bakery/config/SecurityConfig.java
+++ src/main/java/com/onlyflans/bakery/config/SecurityConfig.java
@@ -66,7 +66,8 @@ public class SecurityConfig {
                                 "/h2-console/**",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
-                                "/swagger-ui.html"
+                                "/swagger-ui.html",
+                                "/api/v1/products/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
